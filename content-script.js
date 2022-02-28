53ab9c0df0b21afc88b8888124f5d5d6b7ec2302
Dako Albeik <dako.albeik@gmail.com>
2022-02-27 20:21:18 -0600

    getVideo function complete

--- content-script.js
+++ content-script.js
@@ -1,7 +1,39 @@
-console.log("I ran!");
+console.log("Content Script Ran!");
 
-// const video = document.querySelector('video');
 
-const video = new Video("New Lecture", 50);
+// try to get an HTML video element
+getVideoElement().then((res)=>{
+    console.log("Res:", res.video);
+}).catch((err)=>{
+    console.log("Error!", err);
+})
 
-console.log(video);
+
+
+
+// resolves a promise with an HTML video element
+// or throw an error
+function getVideoElement(repeatCount = 20){
+
+    // return a promise
+    return new Promise((resolve, reject)=>{
+
+    // declare time interval
+    const intervalId = setInterval(()=>{
+
+        // if repeated (repeatCount) times, then reject
+        if(--repeatCount <= 0){
+            clearInterval(intervalId);
+            reject("Failed to find video!");
+        }
+
+        // try to get video again
+        const video = document.querySelector('video');
+        if(video){
+            clearInterval(intervalId);
+            resolve({video});
+        }
+    }, 500);
+
+    })
+}
