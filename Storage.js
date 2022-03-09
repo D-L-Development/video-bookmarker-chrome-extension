9246e8d0cb4903d251f56fa5b7ae17c3bef3228d
Dako Albeik <dako.albeik@gmail.com>
2022-03-08 22:10:30 -0600

    Work on local storage

--- Storage.js
+++ Storage.js
@@ -1,5 +1,46 @@
 class Storage{
     constructor(){
+        this.bookmarks = getBookmarksFromLocalStorage();
+        this.sessionName = this.getSessionNameFromLocalStorage();
+    }
+
+    writeToLocalStorage(key, value){
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    getSessionNameFromLocalStorage(){
+    
+        let sessionName = localStorage.getItem("sessionName");
+        if(sessionName)
+        {
+          return JSON.parse(sessionName);
+        } 
+        else
+        {
+          sessionName = prompt("Please enter a session name:");
+          const newSessionName = {"sessionName": sessionName};
+          writeToLocalStorage("sessionName", newSessionName);
+          return newSessionName;
+        }
+    }
+
+    getBookmarksFromLocalStorage(){
+    
+        let prev_bookmarks = localStorage.getItem("bookmarks");
+        if(prev_bookmarks)
+        {
+          return JSON.parse(prev_bookmarks);
+        } 
+        else
+        {
+          return {}
+        }
         
+    }
+
+    addBookmark(currentTimestamp, bookmarkText){
+        // TODO: check to makes sure there isn't already a timestamp
+        this.bookmarks[currentTimestamp] = bookmarkText;
+        writeToLocalStorage("bookmarks", bookmarks);
     }
 }
