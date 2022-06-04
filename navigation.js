a9f8f764585be01b595a97c018e1b8c0f728aeda
Dako Albeik <dako.albeik@gmail.com>
2022-06-03 21:34:54 -0500

    Make a the scrollable container

--- navigation.js
+++
@@ -1,9 +0,0 @@
-const newSessionButton = document.getElementById("newSessionButton");
-
-newSessionButton.addEventListener("click", (e) => {
-  sendMessageToActiveTab({ action: "createNewSession" }, (response) => {
-    if (response.status === "success") {
-      console.log(`New session created!`);
-    }
-  });
-});
---
+++ uiManager.js
@@ -0,0 +1,3 @@
+class userInterfaceManager {
+  constructor() {}
+}
