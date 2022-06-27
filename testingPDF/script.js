613bc518246758cddfa40b216b09f0a1ad341419
Dako Albeik <dako.albeik@gmail.com>
2022-06-27 16:46:10 -0500

    Add basic table layout

--- testingPDF/script.js
+++ testingPDF/script.js
@@ -3,8 +3,53 @@ const downloadBtnElem = document.querySelector("#saveFile");
 
 const dd = {
   content: [
-    "First paragraph",
-    "Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines",
+    {
+      table: {
+        headerRows: 1,
+        widths: [50, 50, "*"],
+        body: [
+          // header format
+          [
+            {
+              text: "Session Name",
+              colSpan: 3,
+              fontSize: 18,
+              alignment: "center",
+            },
+            "",
+            "",
+          ],
+          // bookmark format
+          [
+            { text: "00:56:08", bold: true },
+            { text: "First title goes here of course", colSpan: 2, bold: true },
+            "",
+          ],
+          [
+            {
+              text: "More information can be found here. So the description of each bookmark goes here. There should be a limit on how long this text should be.",
+              colSpan: 3,
+            },
+            "",
+            "",
+          ],
+          // nested bookmark format
+          [
+            {},
+            { text: "00:56:08", bold: true },
+            { text: "First title goes here of course", bold: true },
+          ],
+          [
+            {},
+            {
+              text: "More information can be found here. So the description of each bookmark goes here. There should be a limit on how long this text should be.",
+              colSpan: 2,
+            },
+            {},
+          ],
+        ],
+      },
+    },
   ],
 };
 
