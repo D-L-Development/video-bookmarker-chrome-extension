1458acec8c74dee8f38b9401a406cdf751611b3a
Dako Albeik <dako.albeik@gmail.com>
2022-07-12 19:16:08 -0500

    Inject global css file for popup

--- react-js-proj/src/components/popup/popup.component.js
+++ react-js-proj/src/components/popup/popup.component.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { StyledPopup } from "./popup.styles";
 
 const PopupComponent = () => {
   return (
-    <div>
-      <h1>PopupComponent</h1>
-    </div>
+    <StyledPopup>
+      <header>
+        <h1>First Lecture</h1>
+        <span id="backArrowIconWrapper">
+          <img
+            src="../images/icons/arrow-left-short.svg"
+            alt="back arrow icon"
+            id="backArrowIcon"
+            class="navIcon"
+          />
+        </span>
+        <img
+          src="../images/icons/x-lg.svg"
+          alt="close icon"
+          id="closeIcon"
+          class="navIcon"
+        />
+      </header>
+    </StyledPopup>
   );
 };
 
---
+++ react-js-proj/src/components/popup/popup.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledPopup = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: white;
+`;
