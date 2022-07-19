eb0786d854c2afd5e30179bb1368a2a30d29dd32
Dako Albeik <dako.albeik@gmail.com>
2022-07-19 17:41:10 -0500

    Add functionlaity to clear icon

--- react-js-proj/src/components/popup/shared/input/input.component.js
+++ react-js-proj/src/components/popup/shared/input/input.component.js
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import PropTypes from "prop-types";
 import * as Styled from "./input.styles";
 import ClearIcon from "../../../../icons/clear-icon/clear.icon";
 import SearchIcon from "../../../../icons/search-icon/search.icon";
 
 const InputComponent = ({ placeholder, marginLeft, marginRight }) => {
+  const [text, setText] = useState("");
+  const [showClearIcon, setShowClearIcon] = useState(false);
+  const inputElem = useRef(null);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+    setShowClearIcon(e.target.value !== "");
+  };
+
+  const handleClear = (e) => {
+    setText("");
+    setShowClearIcon(false);
+    inputElem.current.focus();
+  };
+
   return (
     <Styled.SearchBoxWrapper
       className="SearchBoxWrapper"
       marginLeft={marginLeft}
       marginRight={marginRight}
     >
-      <Styled.SearchBox placeholder={placeholder} />
+      <Styled.SearchBox
+        placeholder={placeholder}
+        value={text}
+        onChange={handleChange}
+        ref={inputElem}
+      />
       <Styled.SearchIconWrapper>
         <SearchIcon width="15px" height="15px" color="#9BA0A5" />
       </Styled.SearchIconWrapper>
-      <Styled.ClearIconWrapper>
-        <ClearIcon width="15px" height="15px" color="white" />
-      </Styled.ClearIconWrapper>
+      {showClearIcon && (
+        <Styled.ClearIconWrapper onClick={handleClear}>
+          <ClearIcon width="15px" height="15px" color="white" />
+        </Styled.ClearIconWrapper>
+      )}
     </Styled.SearchBoxWrapper>
   );
 };
