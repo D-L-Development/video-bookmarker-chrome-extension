edc37be096db14fef20591389654eaa711dbc82b
Dako Albeik <dako.albeik@gmail.com>
2022-07-23 10:24:03 -0500

    Change fake db

--- react-js-proj/src/constants/fake-db.js
+++ react-js-proj/src/constants/fake-db.js
@@ -1,13 +1,28 @@
 export const fakeDB = {
-  ALL_SESSIONS: {
-    ROOT_FOLDERS: [{ uuid: "2g4s-2x8s-2xb6", name: "Game" }],
-    ROOT_SESSIONS: [
+  ROOT: {
+    folders: [{ uuid: "2g4s-2x8s-2xb6", name: "Game" }],
+    sessions: [
       { uuid: "2cvs-658s-2126", name: "Lecture 12", date: "12/01/22" },
     ],
   },
-  // SESSION
+  // FOLDERS
+  "2g4s-2x8s-2xb6": {
+    folders: [
+      { uuid: "212x-228s-2xb6", name: "First Week" },
+      { uuid: "2d2x-c28s-2df1", name: "Second week" },
+    ],
+    sessions: [],
+  },
+  "212x-228s-2xb6": {
+    folders: [],
+    sessions: [],
+  },
+  "2d2x-c28s-2df1": {
+    folders: [],
+    sessions: [],
+  },
+  // SESSIONS
   "2cvs-658s-2126": {
-    name: "Lecture 12",
     bookmarks: {
       "04:32:12": {
         isNested: false,
@@ -16,18 +31,5 @@ export const fakeDB = {
         text: "lorem ipsum",
       },
     },
-  },
-  // FOLDER
-  "2g4s-2x8s-2xb6": {
-    parent: null,
-    name: "Game",
-    folders: ["212x-228s-2xb6"],
-    sessions: [],
-  },
-  "212x-228s-2xb6": {
-    parent: "2g4s-2x8s-2xb6",
-    name: "First Week",
-    folders: [],
-    sessions: [],
   },
 };
--- react-js-proj/src/popup.js
+++ react-js-proj/src/popup.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import PopupComponent from "./components/popup/popup.component";
+import { fakeDB } from "./constants/fake-db";
+
+// TODO: remove this (just for testing)
+document.addEventListener("keydown", (e) => {
+  if (e.key === "r" && e.ctrlKey) {
+    chrome.storage.sync.clear(() => {
+      console.log("CLEARED");
+      chrome.storage.sync.set(fakeDB, () => {
+        console.log("Data set!");
+      });
+    });
+  }
+});
 
 const container = document.getElementById("react-popup-root");
 const root = createRoot(container);
