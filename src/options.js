673d2e73f5560558652d0a85944696f73b8e3fdd
Luka Woodson <llwoodson@ualr.edu>
2022-08-15 23:16:50 -0500

    Fix theme picker bug

--- src/options.js
+++ src/options.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import OptionsComponent from "./components/options/options.component";
-import {ThemeProvider} from "styled-components";
-import {defaultPalettes, THEMES} from "./constants/default-palettes";
+import {ThemePageContextProvider} from "./components/options/context/theme-page-context";
 
 const container = document.getElementById("react-options-root");
 const root = createRoot(container);
-root.render(<ThemeProvider theme={defaultPalettes[THEMES.LIGHT]}><OptionsComponent tab="options" /></ThemeProvider>);
+root.render(<ThemePageContextProvider><OptionsComponent tab="options" /></ThemePageContextProvider>);
