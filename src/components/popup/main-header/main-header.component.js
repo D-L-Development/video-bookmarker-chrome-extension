d9349ad81d0e9f4119304d9a2826410ae5bff507
Dako Albeik <dako.albeik@gmail.com>
2022-08-19 20:01:30 -0500

    remove unused code from main header component

--- src/components/popup/main-header/main-header.component.js
+++ src/components/popup/main-header/main-header.component.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import {
   CloseIconWrapper,
   Header,
@@ -11,60 +11,9 @@ import CogIcon from "../../../icons/cog-icon/cog.icon";
 import CloseIcon from "../../../icons/close-icon/close.icon";
 import { MSG } from "../../../contentScripts/utility";
 
-const OPTIONS_PAGE_URL = chrome.runtime.getURL("./options.html");
-
 const MainHeaderComponent = ({ togglePopup, toggleDrag, updatePopupPos }) => {
-  const mouseData = useRef({
-    mouseUp: true,
-    prevX: 0,
-    prevY: 0,
-    left: 200,
-    top: 200,
-  });
-
-  const portRef = useRef({
-    isLoading: true,
-    port: null,
-  });
-
-  const handleCogIconClick = (e) => {};
-
-  useEffect(() => {}, []);
-
   return (
-    <Header
-    // onMouseDown={(e) => {
-    //   mouseData.current = {
-    //     ...mouseData.current,
-    //     mouseUp: false,
-    //     prevX: e.screenX,
-    //     prevY: e.screenY,
-    //   };
-    // }}
-    // onMouseUp={(e) => {
-    //   mouseData.current = { ...mouseData.current, mouseUp: true };
-    // }}
-    // onMouseMove={(e) => {
-    //   // as long as the mouse is down
-    //   if (mouseData.current.mouseUp) return;
-    //   // calculate new mouse position
-    //   const { screenX, screenY } = e;
-    //   const diffX = mouseData.current.prevX - screenX;
-    //   const diffY = mouseData.current.prevY - screenY;
-    //
-    //   // update actual iframe position
-    //   mouseData.current.left -= diffX;
-    //   mouseData.current.top -= diffY;
-    //   // update prev position
-    //   mouseData.current.prevX = screenX;
-    //   mouseData.current.prevY = screenY;
-    //
-    //   portRef.current.port.postMessage({
-    //     left: mouseData.current.left + "px",
-    //     top: mouseData.current.top + "px",
-    //   });
-    // }}
-    >
+    <Header>
       <HeaderText>Web Video Bookmarker</HeaderText>
       <PopupIconGroup>
         <MainHeaderIconWrapper onClick={toggleDrag}>
